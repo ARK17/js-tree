0cbffa6d06cea04454d3667d0c279ba4d34e3014
Ashikur Rashid Khan <ashikur@linkstaff.co.jp>
2019-01-29 19:48:34 +0900

    logic redefined

--- js/tree.js
+++ js/tree.js
@@ -1,15 +1,20 @@
 function myTreeInit() {
-    createNode("root", "Button", "lvl1Yes", "Yes", "btn-lg btn-warning");
-    createNode("root", "Button", "lvl1No", "No", "btn-lg btn-danger");
+    createDiv("root", "rootDiv");
+    createNode("rootDiv", "Button", "lvl0Yes", "Yes", "btn-lg btn-warning","callYes(\'lvl0\')");
+    createNode("rootDiv", "Button", "lvl0No", "No", "btn-lg btn-danger","callNo(\'lvl0\')");
 }
-function createNode(parentID, elementType, nodeId, text, classes){
+function createDiv(parentID, divId){
+    var div = document.createElement("div");//div
+    div.setAttribute("id", divId);//
+    document.getElementById(parentID).appendChild(div);//root
+}
+function createNode(parentID, elementType, nodeId, text, classes, events){
     var node = document.createElement(elementType);//Button
     node.setAttribute("id", nodeId);//lvl1yes
     node.setAttribute("class",classes);
-    node.setAttribute("onClick", "call"+text+"()");
-    node.onClick = "call"+text+"()";
+    node.setAttribute("onClick", events);//js function name
+    node.onClick = events;
     var textnode = document.createTextNode(text);//Yes
     node.appendChild(textnode);
     document.getElementById(parentID).appendChild(node);//root
-
 }
